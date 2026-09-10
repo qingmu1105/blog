98a11a7190105609fe156f61bd2389559f266af6
qingmu1105 <qingmu1105@163.com>
2026-09-10 16:52:42 +0800

    Revert "Custom domain name"
    This reverts commit 2f35bbc297e786f3d5b460513259c5ab82711cb0.
    
    DNS is not ready.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://haikeyingzhou.com',
+	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{

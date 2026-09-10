5de7934fbbb0fa2573a4f3d51406c2c69d720ccd
qingmu1105 <qingmu1105@163.com>
2026-09-10 17:22:16 +0800

    remove templated content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog';
+export const SITE_DESCRIPTION = '海客谈瀛洲，烟涛微茫信难求。越人语天姥，云霞明灭或可睹。';
